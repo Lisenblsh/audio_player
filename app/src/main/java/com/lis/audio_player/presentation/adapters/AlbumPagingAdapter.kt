3eb25d561e8998e63ce9e29703387803a8f3e65c
lisenblsh <lemi013794@gmail.com>
2022-11-01 23:15:32 +0700

    some

--- app/src/main/java/com/lis/audio_player/presentation/adapters/AlbumPagingAdapter.kt
+++ app/src/main/java/com/lis/audio_player/presentation/adapters/AlbumPagingAdapter.kt
@@ -1,12 +1,54 @@
 package com.lis.audio_player.presentation.adapters
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.lis.audio_player.R
 import com.lis.audio_player.data.room.AlbumDB
 
 class AlbumPagingAdapter: PagingDataAdapter<AlbumDB, RecyclerView.ViewHolder>(ALBUM_COMPARISON) {
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val album = getItem(position)
+        (holder as AlbumViewHolder).bind(album)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.album_list_item, parent, false)
+        return AlbumViewHolder(view)
+    }
+
+    interface OnClickListener{
+        fun onItemClick(id: Long)
+    }
+
+    private lateinit var clickListener: OnClickListener
+
+    fun setOnClickListener(listener: OnClickListener) {
+        this.clickListener = listener
+    }
+
+    inner class AlbumViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        private val image = itemView.findViewById<ImageView>(R.id.album_image)
+        private val title = itemView.findViewById<TextView>(R.id.album_title)
+        private val albumAuthor = itemView.findViewById<TextView>(R.id.album_author)
+
+        fun bind(album: AlbumDB?) {
+
+        }
+
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return R.layout.album_list_item
+    }
 
     companion object {
         private val ALBUM_COMPARISON = object: DiffUtil.ItemCallback<AlbumDB>() {
@@ -15,17 +57,9 @@ class AlbumPagingAdapter: PagingDataAdapter<AlbumDB, RecyclerView.ViewHolder>(AL
             }
 
             override fun areContentsTheSame(oldItem: AlbumDB, newItem: AlbumDB): Boolean {
-                TODO("Not yet implemented")
+                return oldItem == newItem
             }
 
         }
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
     }
 }
